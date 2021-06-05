8fe92f77238def854f8063bad2b8592a5f3816d0
Noel Bray <nwbray-code@yahoo.com>
2021-06-05 12:29:04 -0400

    Add Describe Block "function changeMinValues"
    The describe specification contains two initial it blocks that compare a list of min values to an array, list of expected min value outputs after changeMinValues is invoked within each of them.
    
    Modified File: mocha-in-browser-automated-tests.js

--- mocha-in-browser-automated-tests.js
+++ mocha-in-browser-automated-tests.js
@@ -1,5 +1,77 @@
 describe("Multiple Random Number Generator's Function Tests Suite", function() {
 
+    describe("function changeMinValues", function() {
+
+        it(`If input checkbox element checked attribute's value equals true, set every input element's min attribute to "" that is in the list.`, function() {
+            let checkboxElement = document.createElement('input');
+
+            checkboxElement.type = 'checkbox';
+            checkboxElement.checked = true;
+
+            // console.log("checkboxElement:", checkboxElement);
+
+            let elementList = [];
+
+            for (let i = 0; i < 3; i++) {
+                let inputElement = document.createElement('input');
+
+                inputElement.min = `${i}`;
+
+                elementList.push(inputElement);
+            }
+
+            // console.log("Before changeMinValues: ", elementList);
+
+            // console.log(elementList.map(e => e.min));
+
+            changeMinValues(checkboxElement, elementList);
+
+            // console.log("after changeMinValues: ", elementList);
+
+            // console.log(elementList.map(e => e.min));
+
+            let minValues = elementList.map(element => element.value);
+
+            // expect(elementValues).to.have.all.keys([0, 1, 2]); // Doesn't work.
+
+            // expect(elementValues).to.have.lengthOf(3) // This works.
+
+            expect(minValues).to.deep.equal(["",  "", ""]);
+
+            // assert(false);
+        });
+
+        it(`If input checkbox element's checked attribut equals false, set every input element's min attribute to "0" that is in the list.`, function() {
+            let checkboxElement = document.createElement('input');
+
+            // checkboxElement.checked = false; // Not needed because if there is no checked attribute its default value is false.
+
+            // console.log(checkboxElement.checked);
+
+            let elementList = [];
+
+            for (let i = 0; i < 3; i++) {
+                let inputElement = document.createElement('input');
+
+                inputElement.min = `${i}`;
+
+                elementList.push(inputElement);
+            }
+
+            changeMinValues(checkboxElement, elementList);
+
+            let minValues = elementList.map(element => element.min);
+
+            // expect(minValues).to.equal(["0", "0", "0"]); // Does not work.
+
+            // expect(minValues).to.deep.equal(["0", "0", "0"]); // This works.
+
+            expect(minValues).to.eql(["0", "0", "0"], `The checkboxElement's checked attribute's value may be causing the error, its current boolean value is ${checkboxElement.checked} and should be false`);
+
+            // assert(false);
+        });
+    });
+
     describe("function preventNegativeSign", function() {
 
         it(`If input's, element's value contain a hypen, negative sign "-", at the beginning of the string, replace the "-" with "" and return the new value.`, function() {
